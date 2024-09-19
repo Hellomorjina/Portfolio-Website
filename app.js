bc6a6068d0432b067d92c1f7831586975dcbda37
morjina <fmorjina111@gmail.com>
2024-09-19 19:02:17 +0600

    UP

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ function openmenu(){
 function closemenu(){
   sidemenu.style.transform = 'translateX(16rem)'; 
 }
+
+//top header blur bg
+
+
+const navbar = document.querySelector('nav');
+const navlink = document.querySelector('nav ul');
+
+
+window.addEventListener('scroll',() =>{
+    if(scrollY > 50){
+      navbar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-filter', 'backdrop-blur-lg', 'shadow-sm');
+      navlink.classList.remove('bg-white','shadow-sm','bg-opacity-50',);
+    }else{
+      navbar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-filter', 'backdrop-blur-lg', 'shadow-sm');
+      navlink.classList.add('bg-white','shadow-sm','bg-opacity-50',);
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+
+  theme: {
+    extend:{
+      // gridtemplateColumns:{
+      //   'auto': 'repeat(auto-fit, minmax(200px, 1fr))',
+      //     },
+  fontFamily: {
+    outfit:["Outfit", "system-ui"],
+    ovo:["Ovo", "serif"]
+  },
+  animation:{
+    spin_slow:"spin 6s linear infinite",
+  },
+  color:{
+    lighthover:"#170045",
+    darkhover:"#2a004a",
+  }
+    }
+
+
+}
+}
+
+
+
