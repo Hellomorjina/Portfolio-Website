690568da77a920b1a23db7fcccff23a75114042e
morjina <fmorjina111@gmail.com>
2024-10-24 21:21:25 +0600

    concut

--- app.js
+++ app.js
@@ -23,3 +23,20 @@ window.addEventListener('scroll',() =>{
       navlink.classList.add('bg-white','shadow-sm','bg-opacity-50',);
     }
 })
+
+
+// ------------dark mode------------------
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+function toggleMode() {
+  if ( document.documentElement.classList.contains('dark')){
+      localStorage.theme = 'dark';
+  }else{
+      localStorage.theme = 'light';
+
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ tailwind.config = {
 
   theme: {
     extend:{
-      // gridtemplateColumns:{
-      //   'auto': 'repeat(auto-fit, minmax(200px, 1fr))',
-      //     },
+      gridtemplateColumns:{
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))',
+          },
   fontFamily: {
     outfit:["Outfit", "system-ui"],
     ovo:["Ovo", "serif"]
@@ -13,14 +13,19 @@ tailwind.config = {
     spin_slow:"spin 6s linear infinite",
   },
   color:{
-    lighthover:"#170045",
-    darkhover:"#2a004a",
+    lighthover:'#fcf4ff',
+    darkhover:'#2a004a',
+    darkTheme:'#11001f',
+  },
+  boxShadow:{
+     'black':'4px 4px 0 #000000',
+     'white':'4px 4px 0 #fff',
   }
-    }
+    
 
 
 }
+
 }
-
-
-
+,darkMode: 'selector'
+}
